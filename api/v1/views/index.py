416bcf3c1573bbfc4640fae032503f73f6f90856
Monday Henry <91729119+Hency96@users.noreply.github.com>
2024-04-30 22:01:50 +0100

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,18 +7,23 @@ from flask import jsonify
 from api.v1.views import app_views
 
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route('/status')
 def returnstuff():
     '''return stuff'''
     return jsonify(status='OK')
 
 
-@app_views.route('/stats', strict_slashes=False)
+@app_views.route('/stats')
 def stuff():
     '''JSON Responses'''
-    todos = {'states': State, 'users': User,
-            'amenities': Amenity, 'cities': City,
-            'places': Place, 'reviews': Review}
-    for key in todos:
-        todos[key] = storage.count(todos[key])
+    todos = {
+        'states': State,
+        'users': User,
+        'amenities': Amenity,
+        'cities': City,
+        'places': Place,
+        'reviews': Review
+    }
+    for key, value in todos.items():
+        todos[key] = storage.count(value)
     return jsonify(todos)
